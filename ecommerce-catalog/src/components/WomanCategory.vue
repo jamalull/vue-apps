faf7af3746cd4be016599e65ab634da9e071dc6d
jamalull <jamaluldazzled@gmail.com>
2023-09-20 22:33:43 +0700

    update: Create content for Women Catalog

--- ecommerce-catalog/src/components/WomanCategory.vue
+++ ecommerce-catalog/src/components/WomanCategory.vue
@@ -1,27 +1,28 @@
 <template>
   <div>
-    <table>
-      <th>
-        <td>ID</td>
-        <td>Title</td>
-        <td>Desc</td>
-        <td>Image</td>
-        <td>Price</td>
-        <td>Rating</td>
-        <td>Category</td>
-      </th>
-      <!-- <tr v-for="(item,index) in dataWomen" :key="index">
-        <td>{{ item }}</td> -->
-        <!-- <td>{{ item.title }}</td>
-        <td>{{ item. }}</td>
-        <td>{{ item. }}</td>
-        <td>{{ item. }}</td>
-        <td>{{ item. }}</td>
-        <td>{{ item. }}</td> -->
-      <!-- </tr> -->
-    </table>
+    <div class="productCard">
+      <img :src="dataWomen[index].image" :alt="dataWomen[index].title">
+      <div class="productDetail">
+        <h2 class="title">{{ dataWomen[index].title }}</h2>
+        <div class="category">
+          <p>{{ dataWomen[index].category }}</p>
+          <p>{{ dataWomen[index].rating.rate }}/5</p>
+        </div>
+        <hr>
+        <p class="description">
+          {{ dataWomen[index].description }}
+        </p>
+        <hr>
+        <h2>${{ dataWomen[index].price }}</h2>
+        <div class="btn">
+          <button class="buyNow">Buy Now</button>
+          <button class="nextProduct" @click="nextProduct">Next Product</button>
+        </div>
+      </div>
+    </div>
   </div>
   <div>
+    <br> <br>
     {{ dataWomen[index] }} <br> <br>
     <h1>Index Product ke : {{ index }}</h1>
   </div> <br> <br>
@@ -29,20 +30,23 @@
 </template>
 
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
+  import {onMounted, ref} from 'vue';
 
   interface Women{
-    id:number;
-    title: string;
-    description: string;
-    image: string;
-    price: number;
-    rating: number;
-    category: string;
+    id:number,
+    title: string,
+    description: string,
+    image: string,
+    price: number,
+    rating: {
+      rate:number,
+      count:number
+    },
+    category: string
   }
   
-  let dataWomen = ref<Women[]>([]);
-  let index = ref(0);
+  const dataWomen = ref<Women[]>([]);
+  const index = ref(0);
 
   const getWomenClothing = async () => {
     const api = await fetch("https://fakestoreapi.com/products/category/women's clothing");
@@ -67,7 +71,30 @@ import {onMounted, ref} from 'vue';
   })
 
 </script>
-
+  
 <style>
+  .productCard{
+    display: flex;
+    gap: 50px;
+  }
+  .productCard img {
+    width: 315px;
+    height: 400px; 
+  }
 
+  .productDetail{
+    text-align: left;
+  }
+  .productDetail .category{
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .btn {
+    display: flex;
+    gap: 30px;
+  }
+  .btn button {
+    width: 300px;
+  }
 </style>
